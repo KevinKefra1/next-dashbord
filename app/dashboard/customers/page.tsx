dbf3b787a99d55bc694962912dd458effe714383
kevinkefra1 <kevinkevanck.com>
2024-05-17 03:02:02 +0000

    last

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,32 @@
-export default function Page() {
-    return <p>Customers Page</p>;
+import { fetchCustomers, fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+import { CardsSkeleton } from '@/app/ui/skeletons';
+
+import { Metadata } from 'next';
+import { Suspense } from 'react';
+
+export const metadata: Metadata = {
+    title: 'Customers',
+};
+export default async function Page({
+    searchParams,
+  }: {
+    searchParams?: {
+      query?: string;
+      page?: string;
+    };
+  }) {
+    const query = searchParams?.query || '';
+    const currentPage = Number(searchParams?.page) || 1;
+    const customers = await fetchFilteredCustomers(query);
+  
+    // const customers = await fetchCustomers();
+
+    return (
+        <div>
+            <Suspense  key={query + currentPage} fallback={<CardsSkeleton />}>
+                <CustomersTable customers={customers} />
+            </Suspense>
+        </div>
+    );
 }
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -10,16 +10,18 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   const handleSearch = useDebouncedCallback((term) => {
-    console.log(term); 
+    console.log(term);
     const params = new URLSearchParams(searchParams);
-     if (term) {
+    params.set('page', '1');
+
+    if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
   }, 300);
- 
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
